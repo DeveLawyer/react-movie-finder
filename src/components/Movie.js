91b2c32792d8278f121363623bb685bf3e9dc9ab
Josue Narro <josue.narro.a@gmail.com>
2020-09-29 00:56:50 -0500

    Add default image to poster-less movies

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 const IMG_PATH = `https://image.tmdb.org/t/p/w1280`;
 
-const Movie = ({ title, vote_average, poster_path, overview }) => {
-  const setClassByTitleLength = (title) => (
-    title.length > 40 ? 'small-size' : 'normal-size'
-  )
+const setClassByTitleLength = (title) => (
+  title.length > 40 ? 'small-size' : 'normal-size'
+)
 
-  const setClassByVotes = (votes) => {
-    if (votes >= 8) {
-      return 'green';
-    }
-    if (votes >= 5) {
-      return 'orange';
-    }
-    return 'red';
+const setClassByVotes = (votes) => {
+  if (votes >= 8) {
+    return 'green';
+  }
+  if (votes >= 5) {
+    return 'orange';
+  }
+  return 'red';
 }
 
+const Movie = ({ title, vote_average, poster_path, overview }) => {
   return (
     <div className='movie'>
       <img 
-        src={IMG_PATH + poster_path}
+        src={poster_path ? (IMG_PATH + poster_path) : 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1040&q=80'}
         alt={title}
       />
       <div className='movie-info'>
